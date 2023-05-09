eb5fe12eb9a2ccce31febfb18f47954e371eb070
Gmajorlist <darkmoster@naver.com>
2023-05-09 16:09:41 +0900

    0509

--- day03/src/App.js
+++ day03/src/App.js
@@ -5,6 +5,7 @@ import Test02 from './component/Test02';
 import Test03 from './component/Test03';
 import Test04 from './component/Test04';
 import Test05 from './component/Test05';
+import Test06 from './component/Test06';
 
 const App = () => {
   return (
@@ -13,7 +14,8 @@ const App = () => {
       {/* <Test02/> */}
       {/* <Test03/> */}
       {/* <Test04/> */}
-      <Test05/>
+      {/* <Test05/> */}
+      <Test06/>
     </div>
   );
 };
